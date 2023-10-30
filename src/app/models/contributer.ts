81a1817dffe78bc66ec1d266ada131246201c3fd
DajanaIsaku <isaku.dajana@gmail.com>
2023-10-30 14:44:19 +0100

    vezbe2

---
+++ src/app/models/contributer.ts
@@ -0,0 +1,6 @@
+export class Contributer{
+    id: number;
+    firstName: string;
+    lastName: string;
+    userName: string;
+}
---
+++ src/app/services/tasks/tasks.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { response } from 'express';
+import { Observable, map} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+
+  baseUrl: "https://app.microenv.com/backend/key/934cd5ac5316e3d39833a2/rest/api/tasks/"
+  constructor(private httpClient: HttpClient) {}
+
+    public getTasks() : Observable<any>{
+      return this.httpClient.get(this.baseUrl)
+      .pipe(map((response: Response) => response));
+
+    }
+   
+}
